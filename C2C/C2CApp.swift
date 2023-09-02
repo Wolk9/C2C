6ec4f19bbd0a3c8976c9590848034f225bdac711
Martin op iMac <wolk9@me.com>
2023-09-02 13:52:02 +0200

    Initial Commit

---
+++ C2C/C2CApp.swift
@@ -0,0 +1,17 @@
+//
+//  C2CApp.swift
+//  C2C
+//
+//  Created by Martin de Bes on 02/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct C2CApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
